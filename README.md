ce54ce3a6c03030f7a0749331bb410c08982fcac
matthias4217 <matthias.goffette@gmail.com>
2020-07-19 13:53:25 +0200

    Updates requirements in README ; PageDownloader more or less finished

--- README.md
+++ README.md
@@ -1,7 +1,14 @@
 # wizardawn-toolworld-dl
 
-A small tool to create PNG from world maps generated from https://osricrpg.com/wizardawn/tool_world.php
+A small tool to create PNG from world maps generated from [Wizardawn World Map Generator](https://osricrpg.com/wizardawn/tool_world.php)
 
 Requirements :
   * python3
   * requests
+  * Pillow
+
+Once you have the correct Python version and are in the right venv :
+
+```bash
+$ pip install requests Pillow
+```
--- downloader.py
+++ downloader.py
@@ -2,23 +2,6 @@ import requests
 
 from enum import Enum
 
-## Constants
-
-NAME = "Truc"
-WATER = {
-    'n': 0,
-    'e': 0,
-    's': 0,
-    'w': 0
-}
-INFO = True
-LEGEND = True
-
-WIDTH = 2
-HEIGHT = 1
-
-URL = "https://osricrpg.com/wizardawn/tool_world.php?run=1"
-
 
 class MapGenre(Enum):
     FANTASY = 'Fantasy'
@@ -31,21 +14,66 @@ class MapGenre(Enum):
 
 class MapClimate(Enum):
     COLD_NORTH_WARM_SOUTH = 1
+    WARM_NORTH_COLD_SOUTH = 2
+    COLD_NORTH_COLD_SOUTH = 10
+    WARM_NORTH_WARM_SOUTH = 11
+    SNOW = 3
+    DESERT = 4
+    FOREST = 5
+    JUNGLE = 6
+    TROPICS = 7
+    LIFELESS = 8
+    UNDERWORLD = 12
+    RANDOM = 9
+
+
+class BorderValue(Enum):
+    NO = 0
+    WATER = 1
+    MOUNTAINS = 2
+
+
+# Constants
+
+NAME = "Truc"
+WATER = {
+    'n': BorderValue.MOUNTAINS.value,
+    'e': BorderValue.NO.value,
+    's': BorderValue.NO.value,
+    'w': BorderValue.WATER.value
+}
+INFO = True
+LEGEND = True
+
+COLOR_MAP = True  # whether the tiles are colored
+COLOR_HIGHLIGHTS = False  # whether the points of interest are colored differently
+
+OUTER_HULL = False
+
+WIDTH = 1  # there will be 10*WIDTH hexes on a row
+HEIGHT = 1  # there will be 8*HEIGHT hexes on a column
+
+URL = "https://osricrpg.com/wizardawn/tool_world.php?run=1"
 
 
 class PageDownloader:
     def __init__(self, name=NAME, map_genre=MapGenre.FANTASY.value, map_climate=MapClimate.COLD_NORTH_WARM_SOUTH.value,
-                 height=HEIGHT, width=WIDTH, hex_miles=5, info=INFO, legend=LEGEND, water=WATER):
+                 height=HEIGHT, width=WIDTH, hex_miles=5,
+                 info=INFO, legend=LEGEND, water=WATER, color_map=COLOR_MAP, color_highlights=COLOR_HIGHLIGHTS,
+                 outer_hull=OUTER_HULL):
         self.form_data = {
             'program_user': 1,  # I don't know what this is
             'map_name': name,
             'map_genre': map_genre,
             'map_climate': map_climate,
-            'map_wide': height,
-            'map_high': width,
+            'map_wide': width,
+            'map_high': height,
             'map_equals': hex_miles,
             'info': 1 if info else 0,
             'legend': 1 if legend else 0,
+            'color': 1 if color_map else 0,
+            'colorl': 1 if color_highlights else 0,
+            'hull': 1 if outer_hull else 0,
             'water_n': water['n'],
             'water_s': water['s'],
             'water_e': water['e'],
